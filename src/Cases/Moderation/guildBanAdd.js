b1a6895f6a337e0e9fb7538e5459741e1497d5cf
AbSamad99 <abdussamad.mi5@gmail.com>
2020-08-11 12:14:41 +0530

    Commenting

--- src/Cases/Moderation/guildBanAdd.js
+++ src/Cases/Moderation/guildBanAdd.js
@@ -1,24 +1,24 @@
-/*Handles Logging of whenever a user is banned from the server*/
+// /*Handles Logging of whenever a user is banned from the server*/
 
-//getting the required logging function
-const userBanLog = require('../../Loggers/Moderation/userBanLog.js');
+// //getting the required logging function
+// const userBanLog = require('../../Loggers/Moderation/userBanLog.js');
 
-const UserSchema = require('../../Schemas/UserSchema.js');
+// const UserSchema = require('../../Schemas/UserSchema.js');
 
-const guildBanAddCaseHandler = async (guild, mem) => {
-  let banAuditLog, logsChannel;
+// const guildBanAddCaseHandler = async (guild, mem) => {
+//   let banAuditLog, logsChannel;
 
-  //getting the logs channel
-  logsChannel = guild.channels.cache.get('447513266395283476');
+//   //getting the logs channel
+//   logsChannel = guild.channels.cache.get('447513266395283476');
 
-  //fetching the ban audit log
-  banAuditLog = await guild
-    .fetchAuditLogs({
-      type: 'MEMBER_BAN_ADD',
-    })
-    .then((audit) => audit.entries.first());
+//   //fetching the ban audit log
+//   banAuditLog = await guild
+//     .fetchAuditLogs({
+//       type: 'MEMBER_BAN_ADD',
+//     })
+//     .then((audit) => audit.entries.first());
 
-  userBanLog(banAuditLog, mem, null, logsChannel).catch(console.log);
-};
+//   userBanLog(banAuditLog, mem, null, logsChannel).catch(console.log);
+// };
 
-module.exports = guildBanAddCaseHandler;
+// module.exports = guildBanAddCaseHandler;
